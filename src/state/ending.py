38b1b7eb0a36936f108a72d5bb5017a9339d2ee9
Ema <6emahristova@gmail.com>
2022-05-06 14:49:21 +0300

    soti beshe oburkal imenata na endingite

--- src/state/ending.py
+++ src/state/ending.py
@@ -6,9 +6,9 @@ def show_good_ending():
     UI.prompt(PromptEnding(title="Добрият край", type="good", desc_html="Добри сте, издържахте мандата!"))
 
 
-def show_protest_ending():
+def show_bad_ending():
     UI.prompt(PromptEnding(title="Убит сте в атентат", type="bad", desc_html="Изглежда станахте враг с грешните хора."))
 
 
-def show_bad_ending():
+def show_protest_ending():
     UI.prompt(PromptEnding(title="Край", type="protest", desc_html="Протести избухват в триъгълника на властта и вашата кариера беше дотук."))
